b3425c185901ac3799a8c4111a5262cf70364db0
Davide Riva <driva95@protonmail.com>
2017-06-16 11:17:24 +0200

    When there is only a charater, use char instead of String

--- src/main/java/core/Config.java
+++ src/main/java/core/Config.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 class Config {
     public static final String DEFAULT_FILENAME = "/etc/brooklyn/conf.yml";
@@ -29,133 +30,31 @@ class Config {
     private String dbUri;
 
     public Config(String configFileName) {
-        bots = new LinkedHashMap<String, Object>(0);
-        channels = new LinkedHashMap<String, Object>(0);
-        bridges = new ArrayList(0);
+        this.bots = new LinkedHashMap<String, Object>(0);
+        this.channels = new LinkedHashMap<String, Object>(0);
+        this.bridges = new ArrayList(0);
 
-        fileName = configFileName;
+        this.fileName = configFileName;
     }
 
-    public void load() throws IOException {
-        Yaml yaml = new Yaml();
-
-        InputStream file = new FileInputStream(this.fileName);
-        Object settingsTmp = yaml.load(file);
-        if (!(settingsTmp instanceof Map))
-            throw new IOException("File not formatted correctly");
-
-        Map<String, Object> settings = (Map<String, Object>) settingsTmp;
-
-        try {
-            file.close();
-        } catch (IOException e) {
-            // Should never happen
-            e.printStackTrace();
-        }
-
-        if (!isValid(settings))
-            throw new IOException("File not formatted correctly");
-
-        bots = (Map<String, Object>) settings.get(BOTS_KEY);
-        channels = (Map<String, Object>) settings.get(CHANNELS_KEY);
-        bridges = (ArrayList) settings.get(BRIDGES_KEY);
-        webserver = (Map<String, String>) settings.get(WEBSERVER_KEY);
-    }
-
-    public Map<String, Object> getBots() {
-        return bots;
-    }
-
-    public Map<String, Object> getChannels() {
-        return channels;
-    }
-
-    public ArrayList<ArrayList<String>> getBridges() {
-        return bridges;
-    }
-
-    public Map<String, String> getWebserverConfig() {
-        return webserver;
-    }
-
-    public String getDbUri() {
-        return dbUri;
-    }
-
-    private boolean isValid(Map<String, Object> settings) {
-        if (!settings.containsKey(BOTS_KEY) ||
-                !settings.containsKey(CHANNELS_KEY) ||
-                !settings.containsKey(BRIDGES_KEY) ||
-                !settings.containsKey(WEBSERVER_KEY) ||
-                !settings.containsKey(DATABASE_KEY))
-            return false;
-
-        if (!(settings.get(BOTS_KEY) instanceof Map) ||
-                !(settings.get(CHANNELS_KEY) instanceof Map) ||
-                !(settings.get(BRIDGES_KEY) instanceof ArrayList) ||
-                !(settings.get(WEBSERVER_KEY) instanceof Map))
-            return false;
-
-        Map<String, Object> channels = (Map<String, Object>) settings.get(CHANNELS_KEY);
-        Map<String, Object> bots = (Map<String, Object>) settings.get(BOTS_KEY);
-        ArrayList bridges = (ArrayList) settings.get(BRIDGES_KEY);
-        Map<String, String> webserver = (Map<String, String>) settings.get(WEBSERVER_KEY);
-        this.dbUri = (String) settings.get(DATABASE_KEY);
-
-        if (!isValidBots(bots))
-            return false;
-
-        if (!isValidChannels(channels, bots))
-            return false;
-
-        if (!isValidBridges(bridges, channels))
-            return false;
-
-        return isValidWebserverConfig(webserver);
-    }
-
-    private boolean isValidBots(Map<String, Object> bots) {
+    private static boolean isValidBots(Map<String, Object> bots) {
         for (Object obj : bots.entrySet()) {
-            if (!(obj instanceof Map.Entry))
+            if (!(obj instanceof Entry))
                 return false;
 
-            Map.Entry<String, Object> bot = (Map.Entry<String, Object>) obj;
+            Entry<String, Object> bot = (Entry<String, Object>) obj;
             if (!(bot.getValue() instanceof Map))
                 return false;
 
             Map<String, String> value = (Map<String, String>) bot.getValue();
-            if (!value.containsKey(Config.BOT_TYPE_KEY))
+            if (!value.containsKey(BOT_TYPE_KEY))
                 return false;
         }
 
         return true;
     }
 
-    private boolean isValidChannels(Map<String, Object> channels, Map<String, Object> bots) {
-        for (Object obj : channels.entrySet()) {
-            if (!(obj instanceof Map.Entry))
-                return false;
-
-            Map.Entry<String, Object> channel = (Map.Entry<String, Object>) obj;
-            if (!(channel.getValue() instanceof Map))
-                return false;
-
-            Map<String, String> value = (Map<String, String>) channel.getValue();
-            if (!value.containsKey(BOT_KEY))
-                return false;
-
-            String bot = value.get(BOT_KEY);
-            if (!bots.containsKey(bot))
-                return false;
-
-            if (!value.containsKey(NAME_KEY))
-                return false;
-        }
-
-        return true;
-    }
-
-    private boolean isValidBridges(Iterable bridges, Map<String, Object> channels) {
+    private static boolean isValidBridges(Iterable bridges, Map<String, Object> channels) {
         for (Object obj : bridges) {
             if (!(obj instanceof ArrayList)) {
                 ArrayList<String> bridge = (ArrayList<String>) obj;
@@ -169,8 +68,110 @@ class Config {
         return true;
     }
 
-    private boolean isValidWebserverConfig(Map<String, String> webserver) {
-        return webserver.containsKey(CONTENT_FOLDER_KEY) &&
-                webserver.containsKey(BASE_URL_KEY);
+    private static boolean isValidWebserverConfig(Map<String, String> webserver) {
+        return webserver.containsKey(Config.CONTENT_FOLDER_KEY) &&
+                webserver.containsKey(Config.BASE_URL_KEY);
+    }
+
+    public void load() throws IOException {
+        Yaml yaml = new Yaml();
+
+        InputStream file = new FileInputStream(fileName);
+        Object settingsTmp = yaml.load(file);
+        if (!(settingsTmp instanceof Map))
+            throw new IOException("File not formatted correctly");
+
+        Map<String, Object> settings = (Map<String, Object>) settingsTmp;
+
+        try {
+            file.close();
+        } catch (IOException e) {
+            // Should never happen
+            e.printStackTrace();
+        }
+
+        if (!this.isValid(settings))
+            throw new IOException("File not formatted correctly");
+
+        this.bots = (Map<String, Object>) settings.get(Config.BOTS_KEY);
+        this.channels = (Map<String, Object>) settings.get(Config.CHANNELS_KEY);
+        this.bridges = (ArrayList) settings.get(Config.BRIDGES_KEY);
+        this.webserver = (Map<String, String>) settings.get(Config.WEBSERVER_KEY);
+    }
+
+    public Map<String, Object> getBots() {
+        return this.bots;
+    }
+
+    public Map<String, Object> getChannels() {
+        return this.channels;
+    }
+
+    public ArrayList<ArrayList<String>> getBridges() {
+        return this.bridges;
+    }
+
+    public Map<String, String> getWebserverConfig() {
+        return this.webserver;
+    }
+
+    public String getDbUri() {
+        return this.dbUri;
+    }
+
+    private boolean isValid(Map<String, Object> settings) {
+        if (!settings.containsKey(Config.BOTS_KEY) ||
+                !settings.containsKey(Config.CHANNELS_KEY) ||
+                !settings.containsKey(Config.BRIDGES_KEY) ||
+                !settings.containsKey(Config.WEBSERVER_KEY) ||
+                !settings.containsKey(Config.DATABASE_KEY))
+            return false;
+
+        if (!(settings.get(Config.BOTS_KEY) instanceof Map) ||
+                !(settings.get(Config.CHANNELS_KEY) instanceof Map) ||
+                !(settings.get(Config.BRIDGES_KEY) instanceof ArrayList) ||
+                !(settings.get(Config.WEBSERVER_KEY) instanceof Map))
+            return false;
+
+        Map<String, Object> channels = (Map<String, Object>) settings.get(Config.CHANNELS_KEY);
+        Map<String, Object> bots = (Map<String, Object>) settings.get(Config.BOTS_KEY);
+        ArrayList bridges = (ArrayList) settings.get(Config.BRIDGES_KEY);
+        Map<String, String> webserver = (Map<String, String>) settings.get(Config.WEBSERVER_KEY);
+        dbUri = (String) settings.get(Config.DATABASE_KEY);
+
+        if (!Config.isValidBots(bots))
+            return false;
+
+        if (!this.isValidChannels(channels, bots))
+            return false;
+
+        if (!Config.isValidBridges(bridges, channels))
+            return false;
+
+        return Config.isValidWebserverConfig(webserver);
+    }
+
+    private boolean isValidChannels(Map<String, Object> channels, Map<String, Object> bots) {
+        for (Object obj : channels.entrySet()) {
+            if (!(obj instanceof Entry))
+                return false;
+
+            Entry<String, Object> channel = (Entry<String, Object>) obj;
+            if (!(channel.getValue() instanceof Map))
+                return false;
+
+            Map<String, String> value = (Map<String, String>) channel.getValue();
+            if (!value.containsKey(Config.BOT_KEY))
+                return false;
+
+            String bot = value.get(Config.BOT_KEY);
+            if (!bots.containsKey(bot))
+                return false;
+
+            if (!value.containsKey(Config.NAME_KEY))
+                return false;
+        }
+
+        return true;
     }
 }
