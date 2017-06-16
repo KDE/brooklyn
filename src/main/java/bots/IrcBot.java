a2fe1619ee81d1600617be70915f22d3c9624b2d
Davide Riva <driva95@protonmail.com>
2017-06-16 13:17:15 +0200

    Map(s) are not null at creation, before init()

--- src/main/java/bots/IrcBot.java
+++ src/main/java/bots/IrcBot.java
@@ -13,7 +13,6 @@ import org.kitteh.irc.client.library.element.Channel;
 import org.kitteh.irc.client.library.element.User;
 import org.kitteh.irc.client.library.event.channel.ChannelMessageEvent;
 import org.kitteh.irc.client.library.event.helper.ChannelUserListChangeEvent;
-import org.kitteh.irc.client.library.event.helper.ChannelUserListChangeEvent.Change;
 import org.kitteh.irc.client.library.feature.AuthManager;
 import org.kitteh.irc.client.library.feature.auth.SaslPlain;
 
@@ -33,32 +32,32 @@ public final class IrcBot implements Bot {
     private final Collection<String> usersParticipating = new LinkedHashSet<>();
     private final BotsController botsController = new BotsController();
     private final ResourceBundle resourceBundle = ResourceBundle.getBundle("resources");
-    private Map<String, String> webserverConfig;
+    private final Map<String, String> webserverConfig = new HashMap<>(0);
     private Client client;
     private String botId;
 
     @Override
     public boolean init(String botId, Map<String, String> configs, String[] channels) {
-        if (!configs.containsKey(IrcBot.USERNAME_KEY))
+        if (!configs.containsKey(USERNAME_KEY))
             return false;
-        if (!configs.containsKey(IrcBot.HOST_KEY))
+        if (!configs.containsKey(HOST_KEY))
             return false;
 
-        this.client = Client.builder().nick(configs.get(IrcBot.USERNAME_KEY))
-                .serverHost(configs.get(IrcBot.HOST_KEY)).build();
-        if (configs.containsKey(IrcBot.PASSWORD_KEY)) {
-            AuthManager auth = this.client.getAuthManager();
-            auth.addProtocol(new SaslPlain(this.client,
-                    this.client.getIntendedNick(), configs.get(IrcBot.PASSWORD_KEY)));
+        client = Client.builder().nick(configs.get(USERNAME_KEY))
+                .serverHost(configs.get(HOST_KEY)).build();
+        if (configs.containsKey(PASSWORD_KEY)) {
+            AuthManager auth = client.getAuthManager();
+            auth.addProtocol(new SaslPlain(client,
+                    client.getIntendedNick(), configs.get(PASSWORD_KEY)));
         }
 
-        this.client.getEventManager().registerEventListener(this);
+        client.getEventManager().registerEventListener(this);
 
         for (String channel : channels) {
             try {
-                this.client.addChannel(channel);
+                client.addChannel(channel);
             } catch (IllegalArgumentException e) {
-                System.err.println(String.format("Invalid channel name '%s' on '%s'.", channel, configs.get(IrcBot.HOST_KEY)));
+                System.err.println(String.format("Invalid channel name '%s' on '%s'.", channel, configs.get(HOST_KEY)));
                 e.printStackTrace();
             }
         }
@@ -70,14 +69,14 @@ public final class IrcBot implements Bot {
 
     @Override
     public void addBridge(Bot bot, String channelTo, String channelFrom) {
-        this.botsController.addBridge(bot, channelTo, channelFrom);
+        botsController.addBridge(bot, channelTo, channelFrom);
     }
 
     @Override
     public Optional<String> sendMessage(BotTextMessage msg, String channelTo) {
-        String[] messagesWithoutNewline = IrcBot.COMPILE.split(msg.getText()); // IRC doesn't allow CR / LF
+        String[] messagesWithoutNewline = COMPILE.split(msg.getText()); // IRC doesn't allow CR / LF
         for (String messageToken : messagesWithoutNewline) {
-            this.client.sendMessage(channelTo, BotsController.messageFormatter(
+            client.sendMessage(channelTo, BotsController.messageFormatter(
                     msg.getBotFrom().getId(), msg.getChannelFrom(),
                     msg.getNicknameFrom(), Optional.ofNullable(messageToken)));
         }
@@ -89,16 +88,16 @@ public final class IrcBot implements Bot {
     @Handler(delivery = Invoke.Asynchronously)
     private void onMessageReceived(ChannelMessageEvent message) {
         String authorNickname = message.getActor().getNick();
-        this.usersParticipating.add(authorNickname);
+        usersParticipating.add(authorNickname);
 
         String channelFrom = message.getChannel().getName();
         String text = message.getMessage();
 
-        String[] textSpaceSplitted = IrcBot.PATTERN.split(text);
+        String[] textSpaceSplitted = PATTERN.split(text);
         if (2 == textSpaceSplitted.length &&
-                textSpaceSplitted[0].equals(this.client.getNick()) &&
+                textSpaceSplitted[0].equals(client.getNick()) &&
                 textSpaceSplitted[1].equals("users")) {
-            List<Triplet<Bot, String, List<String>>> users = this.botsController.askForUsers(channelFrom);
+            List<Triplet<Bot, String, List<String>>> users = botsController.askForUsers(channelFrom);
             users.forEach(channel -> {
                 StringBuilder output = new StringBuilder();
                 output.append(channel.getValue0().getClass().getSimpleName())
@@ -109,22 +108,22 @@ public final class IrcBot implements Bot {
                 channel.getValue2().forEach(userTo -> output.append(userTo).append(", "));
 
                 output.delete(output.length() - 2, output.length() - 1);
-                this.client.sendMessage(channelFrom, output.toString());
+                client.sendMessage(channelFrom, output.toString());
             });
         } else {
             BotMessage msg = new BotMessage(authorNickname, channelFrom, this);
             BotTextMessage textMessage = new BotTextMessage(msg, text);
             // An empty msg builder is passed. There aren't reasons to store IRC messages
-            this.botsController.sendMessage(textMessage, channelFrom, Optional.empty());
+            botsController.sendMessage(textMessage, channelFrom, Optional.empty());
         }
     }
 
     @Handler
     public void onJoin(ChannelUserListChangeEvent event) {
         String authorNickname = event.getUser().getNick();
-        if (!authorNickname.equals(this.client.getNick())) {
+        if (!authorNickname.equals(client.getNick())) {
             Optional<Channel> channelFrom = event.getAffectedChannel();
-            Change change = event.getChange();
+            ChannelUserListChangeEvent.Change change = event.getChange();
 
             String channelFromName;
             if (channelFrom.isPresent())
@@ -133,25 +132,25 @@ public final class IrcBot implements Bot {
                 channelFromName = BotsController.EVERY_CHANNEL;
 
             String message;
-            if (0 == change.compareTo(Change.JOIN))
-                message = MessageFormat.format(this.resourceBundle.getString("channel-joined"), authorNickname);
+            if (0 == change.compareTo(ChannelUserListChangeEvent.Change.JOIN))
+                message = MessageFormat.format(resourceBundle.getString("channel-joined"), authorNickname);
             else {
                 // Send a notification only if the user has sent at least one message
-                if (!this.usersParticipating.contains(authorNickname))
+                if (!usersParticipating.contains(authorNickname))
                     return;
 
-                this.usersParticipating.remove(authorNickname);
+                usersParticipating.remove(authorNickname);
                 if (channelFrom.isPresent())
-                    message = MessageFormat.format(this.resourceBundle.getString("channel-leaved"), authorNickname);
+                    message = MessageFormat.format(resourceBundle.getString("channel-leaved"), authorNickname);
                 else
-                    message = MessageFormat.format(this.resourceBundle.getString("server-leaved"), authorNickname);
+                    message = MessageFormat.format(resourceBundle.getString("server-leaved"), authorNickname);
             }
 
             BotMessage msg = new BotMessage(authorNickname, channelFromName, this);
             BotTextMessage textMessage = new BotTextMessage(msg, message);
 
             // A new, useless msg builder is passed. There aren't reasons to store IRC messages
-            this.botsController.sendMessage(textMessage, channelFromName, Optional.empty());
+            botsController.sendMessage(textMessage, channelFromName, Optional.empty());
         }
     }
 
@@ -160,26 +159,26 @@ public final class IrcBot implements Bot {
         try {
             String fileUrl = FileStorage.storeFile(msg.getDoc(), msg.getFileExtension());
             if (msg.getText() != null) {
-                String[] text = IrcBot.COMPILE.split(msg.getText());
+                String[] text = COMPILE.split(msg.getText());
 
                 if (text.length == 1) {
-                    client.sendMessage(channelTo, BotsController.messageFormatter(
+                    this.client.sendMessage(channelTo, BotsController.messageFormatter(
                             msg.getBotFrom().getId(), msg.getChannelFrom(), msg.getNicknameFrom(),
                             Optional.of(fileUrl + ' ' + text[0])));
                 } else {
-                    client.sendMessage(channelTo, BotsController.messageFormatter(
+                    this.client.sendMessage(channelTo, BotsController.messageFormatter(
                             msg.getBotFrom().getId(),
                             msg.getChannelFrom(),
                             msg.getNicknameFrom(),
                             Optional.ofNullable(fileUrl)));
                     for (String messageToken : text) {
-                        this.client.sendMessage(channelTo, BotsController.messageFormatter(
+                        client.sendMessage(channelTo, BotsController.messageFormatter(
                                 msg.getBotFrom().getId(), msg.getChannelFrom(),
                                 msg.getNicknameFrom(), Optional.ofNullable(messageToken)));
                     }
                 }
             } else {
-                client.sendMessage(channelTo, BotsController.messageFormatter(
+                this.client.sendMessage(channelTo, BotsController.messageFormatter(
                         msg.getBotFrom().getId(), msg.getChannelFrom(),
                         msg.getNicknameFrom(), Optional.ofNullable(fileUrl)));
             }
@@ -195,22 +194,22 @@ public final class IrcBot implements Bot {
     @Override
     public void editMessage(BotTextMessage msg, String channelTo, String messageId) {
         String channelName = msg.getBotFrom().channelIdToName(msg.getChannelFrom());
-        String[] messagesWithoutNewline = IrcBot.COMPILE.split(msg.getText()); // IRC doesn't allow CR / LF
+        String[] messagesWithoutNewline = COMPILE.split(msg.getText()); // IRC doesn't allow CR / LF
         for (String messageToken : messagesWithoutNewline) {
-            this.client.sendMessage(channelTo, BotsController.messageFormatter(
+            client.sendMessage(channelTo, BotsController.messageFormatter(
                     msg.getBotFrom().getId(), channelName, msg.getNicknameFrom(),
-                    Optional.of(MessageFormat.format(this.resourceBundle.getString("message-edited"), messageToken))));
+                    Optional.of(MessageFormat.format(resourceBundle.getString("message-edited"), messageToken))));
         }
     }
 
     @Override
     public List<String> getUsers(String channel) {
-        if (this.client.getChannel(channel).isPresent()) {
-            Channel ircChannel = this.client.getChannel(channel).get();
+        if (client.getChannel(channel).isPresent()) {
+            Channel ircChannel = client.getChannel(channel).get();
             List<User> listOfUsers = ircChannel.getUsers();
 
             List<String> output = listOfUsers.stream()
-                    .filter(user -> !user.getNick().equals(this.client.getNick()))
+                    .filter(user -> !user.getNick().equals(client.getNick()))
                     .map(User::getNick)
                     .collect(Collectors.toList());
         }
@@ -219,7 +218,7 @@ public final class IrcBot implements Bot {
 
     @Override
     public String getId() {
-        return this.botId;
+        return botId;
     }
 
     @Override
--- src/main/java/core/Config.java
+++ src/main/java/core/Config.java
@@ -6,8 +6,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 class Config {
     public static final String DEFAULT_FILENAME = "/etc/brooklyn/conf.yml";
@@ -25,28 +27,28 @@ class Config {
     private Map<String, Object> bots;
     private Map<String, Object> channels;
     private ArrayList<ArrayList<String>> bridges;
-    private Map<String, String> webserver;
-    private String dbUri;
+    private Map<String, String> webserver = new HashMap<>(0);
+    private String dbUri = "";
 
     public Config(String configFileName) {
-        bots = new LinkedHashMap<>(0);
-        channels = new LinkedHashMap<>(0);
-        bridges = new ArrayList(0);
+        this.bots = new LinkedHashMap<>(0);
+        this.channels = new LinkedHashMap<>(0);
+        this.bridges = new ArrayList(0);
 
-        fileName = configFileName;
+        this.fileName = configFileName;
     }
 
     private static boolean isValidBots(Map<String, Object> bots) {
         for (Object obj : bots.entrySet()) {
-            if (!(obj instanceof Map.Entry))
+            if (!(obj instanceof Entry))
                 return false;
 
-            Map.Entry<String, Object> bot = (Map.Entry<String, Object>) obj;
+            Entry<String, Object> bot = (Entry<String, Object>) obj;
             if (!(bot.getValue() instanceof Map))
                 return false;
 
             Map<String, String> value = (Map<String, String>) bot.getValue();
-            if (!value.containsKey(BOT_TYPE_KEY))
+            if (!value.containsKey(Config.BOT_TYPE_KEY))
                 return false;
         }
 
@@ -68,14 +70,14 @@ class Config {
     }
 
     private static boolean isValidWebserverConfig(Map<String, String> webserver) {
-        return webserver.containsKey(Config.CONTENT_FOLDER_KEY) &&
-                webserver.containsKey(Config.BASE_URL_KEY);
+        return webserver.containsKey(CONTENT_FOLDER_KEY) &&
+                webserver.containsKey(BASE_URL_KEY);
     }
 
     public void load() throws IOException {
         Yaml yaml = new Yaml();
 
-        InputStream file = new FileInputStream(fileName);
+        InputStream file = new FileInputStream(this.fileName);
         Object settingsTmp = yaml.load(file);
         if (!(settingsTmp instanceof Map))
             throw new IOException("File not formatted correctly");
@@ -89,85 +91,85 @@ class Config {
             e.printStackTrace();
         }
 
-        if (!this.isValid(settings))
+        if (!isValid(settings))
             throw new IOException("File not formatted correctly");
 
-        this.bots = (Map<String, Object>) settings.get(Config.BOTS_KEY);
-        this.channels = (Map<String, Object>) settings.get(Config.CHANNELS_KEY);
-        this.bridges = (ArrayList) settings.get(Config.BRIDGES_KEY);
-        this.webserver = (Map<String, String>) settings.get(Config.WEBSERVER_KEY);
+        bots = (Map<String, Object>) settings.get(BOTS_KEY);
+        channels = (Map<String, Object>) settings.get(CHANNELS_KEY);
+        bridges = (ArrayList) settings.get(BRIDGES_KEY);
+        webserver = (Map<String, String>) settings.get(WEBSERVER_KEY);
     }
 
     public Map<String, Object> getBots() {
-        return this.bots;
+        return bots;
     }
 
     public Map<String, Object> getChannels() {
-        return this.channels;
+        return channels;
     }
 
     public ArrayList<ArrayList<String>> getBridges() {
-        return this.bridges;
+        return bridges;
     }
 
     public Map<String, String> getWebserverConfig() {
-        return this.webserver;
+        return webserver;
     }
 
     public String getDbUri() {
-        return this.dbUri;
+        return dbUri;
     }
 
     private boolean isValid(Map<String, Object> settings) {
-        if (!settings.containsKey(Config.BOTS_KEY) ||
-                !settings.containsKey(Config.CHANNELS_KEY) ||
-                !settings.containsKey(Config.BRIDGES_KEY) ||
-                !settings.containsKey(Config.WEBSERVER_KEY) ||
-                !settings.containsKey(Config.DATABASE_KEY))
+        if (!settings.containsKey(BOTS_KEY) ||
+                !settings.containsKey(CHANNELS_KEY) ||
+                !settings.containsKey(BRIDGES_KEY) ||
+                !settings.containsKey(WEBSERVER_KEY) ||
+                !settings.containsKey(DATABASE_KEY))
             return false;
 
-        if (!(settings.get(Config.BOTS_KEY) instanceof Map) ||
-                !(settings.get(Config.CHANNELS_KEY) instanceof Map) ||
-                !(settings.get(Config.BRIDGES_KEY) instanceof ArrayList) ||
-                !(settings.get(Config.WEBSERVER_KEY) instanceof Map))
+        if (!(settings.get(BOTS_KEY) instanceof Map) ||
+                !(settings.get(CHANNELS_KEY) instanceof Map) ||
+                !(settings.get(BRIDGES_KEY) instanceof ArrayList) ||
+                !(settings.get(WEBSERVER_KEY) instanceof Map))
             return false;
 
-        Map<String, Object> channels = (Map<String, Object>) settings.get(Config.CHANNELS_KEY);
-        Map<String, Object> bots = (Map<String, Object>) settings.get(Config.BOTS_KEY);
-        ArrayList bridges = (ArrayList) settings.get(Config.BRIDGES_KEY);
-        Map<String, String> webserver = (Map<String, String>) settings.get(Config.WEBSERVER_KEY);
-        dbUri = (String) settings.get(Config.DATABASE_KEY);
+        Map<String, Object> channels = (Map<String, Object>) settings.get(CHANNELS_KEY);
+        Map<String, Object> bots = (Map<String, Object>) settings.get(BOTS_KEY);
+        ArrayList bridges = (ArrayList) settings.get(BRIDGES_KEY);
+        Map<String, String> webserver = (Map<String, String>) settings.get(WEBSERVER_KEY);
+        this.dbUri = (String) settings.get(DATABASE_KEY);
 
-        if (!Config.isValidBots(bots))
+        if (!isValidBots(bots))
             return false;
 
-        if (!this.isValidChannels(channels, bots))
+        if (!isValidChannels(channels, bots))
             return false;
 
-        if (!Config.isValidBridges(bridges, channels))
+        if (!isValidBridges(bridges, channels))
             return false;
 
-        return Config.isValidWebserverConfig(webserver);
+        return isValidWebserverConfig(webserver);
     }
 
     private boolean isValidChannels(Map<String, Object> channels, Map<String, Object> bots) {
         for (Object obj : channels.entrySet()) {
-            if (!(obj instanceof Map.Entry))
+            if (!(obj instanceof Entry))
                 return false;
 
-            Map.Entry<String, Object> channel = (Map.Entry<String, Object>) obj;
+            Entry<String, Object> channel = (Entry<String, Object>) obj;
             if (!(channel.getValue() instanceof Map))
                 return false;
 
             Map<String, String> value = (Map<String, String>) channel.getValue();
-            if (!value.containsKey(BOT_KEY))
+            if (!value.containsKey(Config.BOT_KEY))
                 return false;
 
-            String bot = value.get(BOT_KEY);
+            String bot = value.get(Config.BOT_KEY);
             if (!bots.containsKey(bot))
                 return false;
 
-            if (!value.containsKey(NAME_KEY))
+            if (!value.containsKey(Config.NAME_KEY))
                 return false;
         }
 
