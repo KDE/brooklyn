f69726b05bb7e68157d9b6c11c2f55ecb796dff7
Davide Riva <driva95@protonmail.com>
2017-07-05 00:00:29 +0200

    Canaveral lib updated

---
+++ src/main/java/bots/RocketChatBot.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2017 Davide Riva driva95@protonmail.com
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bots;
+
+import core.BotsController;
+import messages.BotDocumentMessage;
+import messages.BotTextMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class RocketChatBot implements Bot {
+    private final BotsController botsController = new BotsController();
+
+    private String botId;
+
+    @Override
+    public boolean init(String botId, Map<String, String> configs, String[] channels) {
+        this.botId = botId;
+
+        return false;
+    }
+
+    @Override
+    public void addBridge(Bot bot, String channelTo, String channelFrom) {
+        botsController.addBridge(bot, channelTo, channelFrom);
+    }
+
+    @Override
+    public Optional<String> sendMessage(BotTextMessage msg, String channelTo) {
+        // TODO: implement that
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<String> sendMessage(BotDocumentMessage msg, String channelTo) {
+        // TODO: implement that
+        return Optional.empty();
+    }
+
+    @Override
+    public void editMessage(BotTextMessage msg, String channelTo, String messageId) {
+
+    }
+
+    @Override
+    public List<String> getUsers(String channel) {
+        // TODO: implement that
+        return new ArrayList<>(0);
+    }
+
+    @Override
+    public String getId() {
+        return botId;
+    }
+
+    @Override
+    public String channelIdToName(String channelId) {
+        // TODO: implement that
+        return channelId;
+    }
+}
